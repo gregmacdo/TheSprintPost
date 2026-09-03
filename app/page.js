e40cda310c2eb7065c5c8c0b487b5f78fbb1ab4d
gregmacdo <79952656+gregmacdo@users.noreply.github.com>
2026-09-03 15:06:00 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 export default async function Page() {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
   const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
