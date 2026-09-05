453d81a02d27417f9e143391fae3612d9a277b49
gregmacdo <79952656+gregmacdo@users.noreply.github.com>
2026-09-04 17:59:50 -0700

    Create route.js

---
+++ app/auth/callback/route.js
@@ -0,0 +1,18 @@
+// app/auth/callback/route.js
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+export async function GET(request) {
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get('code');
+
+  if (code) {
+    const cookieStore = cookies();
+    const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  // Redirect back to home dashboard after successful sign in
+  return NextResponse.redirect(requestUrl.origin);
+}
